24c624c45e540115b723a8b8e5b74751cfe2f780
Tree Some <youn@tree-some.dev>
2020-09-14 11:39:58 +0900

    bug fix

--- meta-data.js
+++ meta-data.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 /*
  * meta-data.js
  * Created on Tue Aug 04 2020
@@ -9,34 +8,36 @@
 const path = require('path');
 const fs = require('fs');
 
-const Hexo = require('hexo');
-const hexo = new Hexo(process.cwd(), {});
+const MHexo = require('hexo');
+const mhexo = new MHexo(process.cwd(), {});
 
 const db = [];
 
-(async () => {
-	await hexo.init();
-	await hexo.load();
+mhexo.init()
+	.then(() => mhexo.load())
+	.then(() => {
+		const sourceDir = mhexo.config.source_dir;
+		const query = mhexo.locals.get('posts');
+		for ( let i=0; i < query.length;i++ ) {
+			const post = query.data[i];
+			const utc = post.updated.utc().format();
 
-	const sourceDir = hexo.config.source_dir;
-	const query = hexo.locals.get('posts');
-	for ( let i=0; i < query.length;i++ ) {
-		const post = query.data[i];
-		const utc = post.updated.utc().format();
+			db.push({
+				title: post.title,
+				href: post.path,
+				updated: utc,
+				src: path.join(sourceDir, post.path),
+				categories: post.categories.data,
+				tags: post.tags.data,
+			});
+		}
 
-		db.push({
-			title: post.title,
-			href: post.path,
-			updated: utc,
-			src: path.join(sourceDir, post.path),
-			categories: post.categories.data,
-			tags: post.tags.data,
-		});
-	}
+		fs.writeFileSync(
+			path.join(process.cwd(), 'meta-data.json'),
+			JSON.stringify(db, null, '\t'),
+			{ encoding: 'utf8' }
+		);
+		mhexo.exit();
+		process.exit(0);
+	});
 
-	fs.writeFileSync(
-		path.join(__dirname, 'meta-data.json'),
-		JSON.stringify(db, null, '\t'),
-		{ encoding: 'utf8' }
-	);
-})();
