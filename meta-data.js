a210b4caa0611dff0794d38ec4ac204fe43e9fd4
raravel <youn@tree-some.dev>
2021-05-24 14:34:04 +0900

    cover가 로컬 주소이면 url형태로 변경

--- meta-data.js
+++ meta-data.js
@@ -21,6 +21,12 @@ const dateSort = (a, b) => {
 	return bd.getTime() - ad.getTime();
 };
 
+// https://stackoverflow.com/questions/8667070/javascript-regular-expression-to-validate-url
+function validateUrl(value) {
+	return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+}
+
+
 const MAX_CONTENT_LENGTH = 50;
 
 mhexo.init()
@@ -40,6 +46,17 @@ mhexo.init()
 				content = content.substr(0, MAX_CONTENT_LENGTH) + ' ...';
 			}
 
+			let cover = post.cover;
+			if ( cover && !validateUrl(cover) ) {
+				const url = mhexo.config.url;
+				let suffix = '';
+				if ( url[url.length-1] !== '/' && post.cover[0] !== '/' ) {
+					suffix = '/';
+				}
+
+				cover = `${mhexo.config.url}${suffix}${cover}`;
+			}
+
 			db.push({
 				title: post.title,
 				href: post.path,
@@ -48,7 +65,7 @@ mhexo.init()
 				src: path.join(sourceDir, post.source),
 				categories: post.categories.data.map(c => c.name),
 				tags: post.tags.data.map(t => t.name),
-				cover: post.cover,
+				cover,
 				content,
 			});
 		}
